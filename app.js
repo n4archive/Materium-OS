f36e758fdf80377504c1ba1ba1745fc66cc20555
sreedharsanthosh <sreedharsanthosh2005@gmail.com>
2021-10-10 14:57:55 +0530

    made slight animations

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ hamburger.addEventListener("click", () => {
   const navLinks = document.querySelector(".nav-links");
   navLinks.classList.toggle("nav-active");
 });
+
+gsap.to(".slider", { opacity: 0, delay: 1.5, duration: 0.7 });
+
+gsap.to(".home-text", { opacity: 1, delay: 1.8, duration: 0.7 });
+gsap.to(".home-image img", {
+  opacity: 1,
+  delay: 1.8,
+  duration: 0.7,
+});
