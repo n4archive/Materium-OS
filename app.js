71cc4a1663d84956160596543543cacf4f377fd1
sreedharsanthosh <sreedharsanthosh2005@gmail.com>
2021-10-29 21:04:17 +0530

    made navBar Changes

--- app.js
+++ app.js
@@ -1,39 +1,27 @@
 const hamburger = document.querySelector(".hamburger");
-const list = document.querySelectorAll(".nav-links li");
-const navLinks = document.querySelector(".nav-links");
+const list = document.querySelectorAll(".overlay-nav li");
+const navLinks = document.querySelector(".nav-overlay");
 
 hamburger.addEventListener("click", () => {
+  const navLinks = document.querySelector(".nav-overlay");
   navLinks.classList.toggle("nav-active");
+  hamburger.classList.toggle("hamburger-active");
 });
 
 list.forEach((li) => {
   li.addEventListener("click", () => {
     navLinks.classList.toggle("nav-active");
+    hamburger.classList.toggle("hamburger-active");
   });
 });
 
 gsap.to(".slider", { opacity: 0, delay: 1.5, duration: 0.7 });
 
-gsap.to(".home-text", { opacity: 1, delay: 1.8, duration: 0.7 });
+gsap.to(".home-text", { opacity: 1, x: 0, delay: 1.8, duration: 0.7 });
+gsap.to(".badge", { opacity: 1, x: 0, delay: 1.5, duration: 0.7 });
 gsap.to(".home-image img", {
   opacity: 1,
+  x: 0,
   delay: 1.8,
   duration: 0.7,
 });
-
-// const swiper = new Swiper(".swiper", {
-//   // Optional parameters
-//   direction: "horizontal",
-//   loop: true,
-
-//   // If we need pagination
-//   pagination: {
-//     el: ".swiper-pagination",
-//   },
-
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
